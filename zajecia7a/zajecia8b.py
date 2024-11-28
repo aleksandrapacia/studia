e443d8724ec692ec65561e4ba768931a8e1f275e
426846  WGGiIŚ <426846@geod.local>
2024-11-28 19:32:05 +0100

    call class function

--- zajecia7a/zajecia8b.py
+++ zajecia7a/zajecia8b.py
@@ -12,6 +12,9 @@ class Ulamek():
         self.m=mianownik
         self.l=licznik
 
+    def to_str(self):
+        return f"To jest ułamek ,który ma licznik = {self.l} i mianownik ={self.m}"
+
 ulamek1 = Ulamek(1,2)
 ulamek2 = Ulamek(2, 4)
 print(ulamek1.m)
@@ -19,3 +22,4 @@ print(ulamek1.l)
 ulamek1.l = 3
 print(ulamek1.l)
 print(ulamek1) #objekt w miejscu pamieci
+print(ulamek1.to_str())
